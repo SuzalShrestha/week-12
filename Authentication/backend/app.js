b0f97b8c1ee2369f01afaf1014ed104bbdafd9cd
Sujal Shrestha <sujal.scxi121@amv.edu.np>
2023-07-14 12:00:01 +0545

    added setTokenCookie, restoreUser,requireAuth functions in auth utils in phase 3

--- Authentication/backend/app.js
+++ Authentication/backend/app.js
@@ -12,6 +12,30 @@ const app = express();
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(express.json());
+
+// Security Middleware
+if (!isProduction) {
+  // enable cors only in development
+  app.use(cors());
+}
+// helmet helps set a variety of headers to better secure your app
+app.use(
+  helmet.crossOriginResourcePolicy({
+    policy: "cross-origin",
+  })
+);
+// Set the _csrf token and create req.csrfToken method
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true,
+    },
+  })
+);
+const routes = require("./routes");
+app.use(routes); // Connect all the routes
 // Catch unhandled requests and forward to error handler.
 app.use((_req, _res, next) => {
   const err = new Error("The requested resource couldn't be found.");
@@ -39,27 +63,4 @@ app.use((err, _req, res, _next) => {
     stack: isProduction ? null : err.stack,
   });
 });
-// Security Middleware
-if (!isProduction) {
-  // enable cors only in development
-  app.use(cors());
-}
-// helmet helps set a variety of headers to better secure your app
-app.use(
-  helmet.crossOriginResourcePolicy({
-    policy: "cross-origin",
-  })
-);
-// Set the _csrf token and create req.csrfToken method
-app.use(
-  csurf({
-    cookie: {
-      secure: isProduction,
-      sameSite: isProduction && "Lax",
-      httpOnly: true,
-    },
-  })
-);
-const routes = require("./routes");
-app.use(routes); // Connect all the routes
 module.exports = app;
--- Authentication/backend/routes/api/index.js
+++ Authentication/backend/routes/api/index.js
@@ -1,6 +1,27 @@
 // backend/routes/api/index.js
 const router = require("express").Router();
-router.post("/test", function (req, res) {
-  res.json({ requestBody: req.body });
-});
+//testing the api
+// router.post("/test", function (req, res) {
+//   res.json({ requestBody: req.body });
+// });
+// GET /api/set-token-cookie
+// const { setTokenCookie } = require("../../utils/auth.js");
+// const { User } = require("../../db/models");
+// router.get("/set-token-cookie", async (_req, res) => {
+//   const user = await User.findOne({
+//     where: {
+//       username: "Demo-lition",
+//     },
+//   });
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// });
+// const { restoreUser } = require("../../utils/auth.js");
+// router.get("/restore-user", restoreUser, (req, res) => {
+//   return res.json(req.user);
+// });
+// const { requireAuth } = require("../../utils/auth.js");
+// router.get("/require-auth", requireAuth, (req, res) => {
+//   return res.json(req.user);
+// });
 module.exports = router;
